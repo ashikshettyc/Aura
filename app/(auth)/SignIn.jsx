11180ff012ecfb8ccc382c2b5bdc2c6ccd0ce7c6
Ashik <ashikshettyc@gmail.com>
2024-08-20 21:02:50 +0530

    sign in and sign up added

--- app/(auth)/SignIn.jsx
+++ app/(auth)/SignIn.jsx
@@ -2,15 +2,21 @@ import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
 import { images } from "../../constants"
 import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 const SignIn = () => {
     const [form, setForm] = useState({
         email:"",
         password:""
     })
+const [isSubmitting, setIsSubmitting] = useState(false)
+    const submit = () => {
+
+    }
   return (
     <SafeAreaView className="bg-primary h-full">
         <ScrollView>
-            <View className="w-full justify-center h-full px-4 my-6">
+            <View className="w-full justify-center min-h-[83vh] px-4 my-6">
             <Image source={images.logo}
             resizeMode='contain' 
             className="w-[115px] h-[35px]"/>
@@ -31,6 +37,15 @@ const SignIn = () => {
             otherStyles="mt-7"
             
             />
+
+            <CustomButton title="Sign In"
+            handlePress={submit}
+            containerStyle="mt-7"
+            />
+            <View className="justify-center pt-5 flex-row gap-2">
+<Text className="text-lg text-gray-100 font-pregular">Don't have account?</Text>
+<Link href="/SignUp" className='text-lg font-psemibold text-secondary'>SIgn Up</Link>
+            </View>
             </View>
         </ScrollView>
     </SafeAreaView>
--- app/(auth)/SignUp.jsx
+++ app/(auth)/SignUp.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { images } from "../../constants"
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
+const SignUp = () => {
+    const [form, setForm] = useState({
+      userName:"",  
+      email:"",
+        password:""
+    })
+const [isSubmitting, setIsSubmitting] = useState(false)
+    const submit = () => {
 
-function SignUp() {
+    }
   return (
-    <div>SignUp</div>
+    <SafeAreaView className="bg-primary h-full">
+        <ScrollView>
+            <View className="w-full justify-center min-h-[83vh] px-4 my-6">
+            <Image source={images.logo}
+            resizeMode='contain' 
+            className="w-[115px] h-[35px]"/>
+            <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+                SignUp to Aora
+            </Text>
+            <FormField
+            title="UserName"
+            value={form.userName}
+            handleChangeText={(e) => setForm({ ...form, userName:e})}
+            otherStyles="mt-10"
+          
+            />
+            <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email:e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            />
+            <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password:e})}
+            otherStyles="mt-7"
+            
+            />
+
+            <CustomButton title="Sign Up"
+            handlePress={submit}
+            containerStyle="mt-7"
+            />
+            <View className="justify-center pt-5 flex-row gap-2">
+<Text className="text-lg text-gray-100 font-pregular">Have an Account Already?</Text>
+<Link href="/SignIn" className='text-lg font-psemibold text-secondary'>SIgn In</Link>
+            </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+
   )
 }
 
