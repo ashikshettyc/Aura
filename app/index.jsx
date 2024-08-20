60bcb5ce2eca0810adf7da6f7a29992652787e16
ashik <ashikshettyc@gmail.com>
2024-08-20 09:30:30 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,41 @@
+import { Link, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4 ">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+<Text className="text-3xl text-white font-bold text-center">Discover Endless Possibility with { " "} 
+  <Text className="text-secondary-200">Aora</Text>
+</Text>
+<Image source={images.path}
+className="w-[136px] h-[15px] absolute -bottom-2 -right-8" resizeMode="contain"
+/>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center w-11/12">Where creativity meets innovation:
+embark on the journey of limitless exploration with Aora
+          </Text>
+          <CustomButton title="Continue with Email"
+          handlePress={() => router.push('/SignIn')}
+          containerStyle="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light"/>
+    </SafeAreaView>
+  );
+}
